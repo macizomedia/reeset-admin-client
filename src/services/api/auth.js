20601fd15771f8c75a384c4b48dfcd52cd2f71e2
Blas Oronoz <info@macizomedia.com>
2020-06-12 09:40:04 +0200

    fix no-console error

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export default {
   userLogin(payload) {
     return axios.post('/login', payload)
-    console.log(payload)
   },
   refreshToken() {
     return axios.get('/auth/token')
